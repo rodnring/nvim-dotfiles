68076af28a3c1d373029e2d6bb1c890315825f1c
Aryan Shamsinejad <aryan.shamsinejad@gmail.com>
2025-01-29 11:46:30 +0330

    work branch

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "lua_ls", "ts_ls", "tailwindcss", "astro", "rust_analyzer", "svelte", "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -19,6 +19,6 @@ end
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
+
 --   capabilities = nvlsp.capabilities,
 -- }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,95 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "ggandor/lightspeed.nvim",
+    lazy = false,
+  },
+  {
+    "tveskag/nvim-blame-line"
+  },
+  {
+    "NeogitOrg/neogit",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css", "tsx", "jsdoc",
+        "json", "json5", "scss"
+      },
+    },
+  },
+  {
+    "karb94/neoscroll.nvim",
+    keys = { "<C-d>", "<C-u>" },
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
+  { 'akinsho/git-conflict.nvim', version = "*", config = true },
+  {
+    "folke/twilight.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "folke/zen-mode.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+    },
+    {
+      "monkoose/neocodeium",
+      event = "VeryLazy",
+      config = function()
+        local neocodeium = require("neocodeium")
+        neocodeium.setup()
+        vim.keymap.set("i", "<A-f>", neocodeium.accept)
+      end,
+    },
+    {
+      "github/copilot.vim"
+    },
+  }
 }
