f9f5cfa3038fd340a4976b95e880ba4adaa37682
Aryan Shamsinejad <aryan.shamsinejad@gmail.com>
2025-02-07 00:22:57 +0330

    (WIP): Updated some stuff, added new file and plugins. git commit -m "Updated lazy-lock.json, lua/configs/codecompanion.lua, lua/configs/fidget-spinner.lua, lua/configs/minuet-ai.lua, and lua/mappings.lua. Added lua/configs/avante.lua, lua/configs/blink-cmp.lua, and updated lua/plugins/init.lua."

--- lua/configs/codecompanion.lua
+++ lua/configs/codecompanion.lua
@@ -1,21 +1,69 @@
-require("codecompanion").setup({
+local options = {
   strategies = {
     chat = {
-      adapter = 'openai'
+      adapter = "my_custom_service",
     },
     inline = {
-      adapter = 'openai'
+      adapter = "my_custom_service",
     },
   },
+
   adapters = {
     openai = function()
+      return require("codecompanion.adapters").extend("openai", {
+        schema = {
+          model = {
+            default = "gpt-4o-mini",
+          },
+        },
+      })
+    end,
+
+    my_custom_service = function()
       return require("codecompanion.adapters").extend("openai_compatible", {
+        schema = {
+          model = {
+            default = "gpt-4o-mini"
+          }
+        },
         env = {
-          url = "https://api.avalai.ir", -- optional: default value is ollama url http://127.0.0.1:11434
-          api_key = "aa-gbzkMh6Voq5LCjAKVqmDgESrhMnaSAs5CDHEVR449RZOkFfN",               -- optional: if your endpoint is authenticated
-          chat_url = "/v1/",        -- optional: default value, override if different
+          url = "https://api.avalai.ir",
+          api_key = "aa-gbzkMh6Voq5LCjAKVqmDgESrhMnaSAs5CDHEVR449RZOkFfN",
+          chat_url = "/v1/chat/completions", -- optional: default value, override if different
+          model = "gpt-4o-mini",
         },
       })
     end,
   },
-})
+}
+
+return options
+
+
+--
+-- require("codecompanion").setup({
+--   strategies = {
+--     chat = {
+--       adapter = 'openai_compatible'
+--     },
+--     inline = {
+--       adapter = 'openai_compatible'
+--     },
+--   },
+--   adapters = {
+--     openai_compatible = function()
+--       return require("codecompanion.adapters").extend("openai_compatible", {
+--         schema = {
+--           model = {
+--             default = 'gpt-4o-mini'
+--           },
+--         },
+--         env = {
+--           url = "https://api.avalai.ir", -- optional: default value is ollama url http://127.0.0.1:11434
+--           api_key = "aa-gbzkMh6Voq5LCjAKVqmDgESrhMnaSAs5CDHEVR449RZOkFfN",               -- optional: if your endpoint is authenticated
+--           -- chat_url = "/v1/",        -- optional: default value, override if different
+--         },
+--       })
+--     end,
+--   },
+-- })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,6 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
@@ -75,131 +74,255 @@ return {
     lazy = false,
     opts = {},
   },
-  {
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim", -- required by telescope
-      "MunifTanjim/nui.nvim",
-
-      -- optional
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      -- configuration goes here
-    },
-  },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {
-      provider = "ollama",
-      vendors = {
-        ollama = {
-          __inherited_from = "openai",
-          endpoint = 'http://127.0.0.1:11434/v1',
-          model = 'qwen2.5-coder:7b',
-        }
-      },
-      auto_suggestions_provider = "ollama",
-
-      -- provider = "ollama",
-      -- vendors = {
-      --   ollama = {
-      --     __inherited_from = "openai",
-      --     endpoint = 'http://127.0.0.1:11434/v1',
-      --     model = 'deepseek-r1:14b',
-      --   }
-      -- },
-      -- provider = "openai",
-      -- openai = {
-      --   endpoint = 'https://api.avalai.ir/v1',
-      --   model = 'gpt-4o-mini-2024-07-18',
-      -- },
-      -- auto_suggestions_provider = "openai",
-      -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",        -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
   -- {
-  --   "olimorris/codecompanion.nvim",
+  --   "kawre/leetcode.nvim",
+  --   build = ":TSUpdate html",
   --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --     "nvim-lua/plenary.nvim", -- required by telescope
+  --     "MunifTanjim/nui.nvim",
+  --
+  --     -- optional
   --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-tree/nvim-web-devicons",
   --   },
-  --   config = function()
-  --     local codecompanion = require("codecompanion")
-  --     codecompanion.setup({
-  --       strategies = {
-  --         chat = {
-  --           adapter = 'openai'
-  --         },
-  --         inline = {
-  --           adapter = 'openai'
-  --         },
-  --       },
-  --       adapters = {
-  --         openai = function()
-  --           return require("codecompanion.adapters").extend("openai_compatible", {
-  --             env = {
-  --               url = "https://api.avalai.ir/v1",                                -- optional: default value is ollama url http://127.0.0.1:11434
-  --               api_key = "aa-gbzkMh6Voq5LCjAKVqmDgESrhMnaSAs5CDHEVR449RZOkFfN", -- optional: if your endpoint is authenticated
-  --             },
-  --           })
-  --         end,
-  --       },
-  --     })
-  --   end
+  --   opts = {
+  --     -- configuration goes here
+  --   },
   -- },
   -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  --   opts = {
+  --     vendors = {
+  --       ollama = {
+  --         __inherited_from = "openai",
+  --         endpoint = 'http://127.0.0.1:11434/v1',
+  --         model = 'qwen2.5-coder:7b',
+  --       },
+  --       custom_config = {
+  --         __inherited_from = "openai",
+  --         endpoint = 'https://api.avalai.ir/v1',
+  --         model = 'gpt-4o-mini'
+  --       }
+  --     },
+  --     -- add any opts here
+  --     -- for example
+  --     provider = "ollama",
+  --     openai = {
+  --       endpoint = 'https://api.avalai.ir/v1',
+  --       model = 'gpt-4o-mini',
+  --       timeout = 30000, -- timeout in milliseconds
+  --       temperature = 0, -- adjust if needed
+  --       max_tokens = 4096,
+  --     },
+  --     auto_suggestions_provider = "ollama",
+  --     behaviour = {
+  --       auto_suggestions = true,
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+  --     "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+  --     "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua",        -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
+  {
+    "olimorris/codecompanion.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "j-hui/fidget.nvim",
+    },
+    config = function()
+      require('codecompanion').setup({
+        display = {
+          action_palette = {
+            width = 95,
+            height = 10,
+            prompt = "Prompt ",                   -- Prompt used for interactive LLM calls
+            provider = "default",                 -- default|telescope|mini_pick
+            opts = {
+              show_default_actions = true,        -- Show the default actions in the action palette?
+              show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+            },
+          },
+        },
+        opts = {
+          log_level = "DEBUG", -- or "TRACE"
+        },
+        adapters = {
+          ollama_custom = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              name = "ollama_custom",
+              formatted_name = "Custom Ollama Interface",
+              env = {
+                api_key = "OLLAMA",
+              },
+            })
+          end,
+          avalai_ollama = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              name = "avalai_ollama",
+              formatted_name = "Ollama Interface (AvalAi)",
+              schema = {
+                model = {
+                  default = "gpt-4o-mini",
+                },
+              },
+              env = {
+                url = 'https://api.avalai.ir',
+                api_key = "OPENAI_API_KEY",
+              },
+            })
+          end,
+          openai_compatible_customized = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              name = "openai_compatible_customized",
+              formatted_name = "Custom Endpoint OpenAI",
+              schema = {
+                model = {
+                  default = "gpt-4o-mini",
+                },
+              },
+              env = {
+                url = 'https://api.avalai.ir',
+                api_key = "OPENAI_API_KEY",
+              },
+            })
+          end,
+        },
+        strategies = {
+          chat = {
+            adapter = "ollama_custom",
+          },
+          inline = {
+            adapter = "ollama_custom"
+          }
+        },
+        init = function()
+          require("configs.fidget-spinner"):init()
+        end
+      })
+    end
+    -- opts = require "configs.codecompanion"
+  },
+  -- -- {
   --   "monkoose/neocodeium",
   --   event = "VeryLazy",
   --   config = function()
   --     local neocodeium = require("neocodeium")
   --     neocodeium.setup()
   --   end,
-  -- }
+  -- },
+  {
+    'milanglacier/minuet-ai.nvim',
+    lazy = false,
+    config = function()
+      require "configs.minuet-ai"
+    end,
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      require('cmp').setup {
+        sources = {
+          {
+            -- Include minuet as a source to enable autocompletion
+            { name = 'minuet' },
+            -- and your other sources
+          }
+        },
+        performance = {
+          -- It is recommended to increase the timeout duration due to
+          -- the typically slower response speed of LLMs compared to
+          -- other completion sources. This is not needed when you only
+          -- need manual completion.
+          fetching_timeout = 2000,
+        },
+      }
+
+
+      -- If you wish to invoke completion manually,
+      -- The following configuration binds `A-y` key
+      -- to invoke the configuration manually.
+      require('cmp').setup {
+        mapping = {
+          ["<A-m>"] = require('minuet').make_cmp_map()
+          -- and your other keymappings
+        },
+      }
+    end
+  },
+  -- {
+  --   'Saghen/blink.cmp',
+  --   lazy = false,
+  --   require('blink-cmp').setup {
+  --     version = '*',
+  --     opts = {
+  --       keymap = { preset = 'super-tab' },
+  --       appearance = {
+  --         use_nvim_cmp_as_default = true,
+  --       },
+  --     },
+  --
+  --     keymap = {
+  --       -- Manually invoke minuet completion.
+  --       ['<A-m>'] = require('minuet').make_blink_map(),
+  --     },
+  --     sources = {
+  --       -- Enable minuet for autocomplete
+  --       default = { 'lsp', 'path', 'buffer', 'snippets', 'minuet' },
+  --       -- For manual completion only, remove 'minuet' from default
+  --       providers = {
+  --         minuet = {
+  --           name = 'minuet',
+  --           module = 'minuet.blink',
+  --           score_offset = 8, -- Gives minuet higher priority among suggestions
+  --         },
+  --       },
+  --     },
+  --     -- Recommended to avoid unnecessary request
+  --     completion = { trigger = { prefetch_on_insert = false } },
+  --   }
+  -- },
 }
